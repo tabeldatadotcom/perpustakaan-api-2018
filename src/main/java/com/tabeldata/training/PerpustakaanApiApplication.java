8399b6376478d1fb266a3ad8afa34fcd28346267
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-12-05 13:36:48 +0700

    init project

---
+++ src/main/java/com/tabeldata/training/PerpustakaanApiApplication.java
@@ -0,0 +1,12 @@
+package com.tabeldata.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerpustakaanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerpustakaanApiApplication.class, args);
+	}
+}
